5ebe367368db59f505a60b67685825c4d7d7f27a
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-03 01:06:18 -0800

    Made a game

---
+++ questions.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    text: `What is my favorite color?
+      A. Purple
+      B. Pink
+      C. Cobalt blue
+      D. White`,
+    correctAnswer: "C"
+  },
+  {
+    text: `What's is my puppy's name?
+      A. Stinky
+      B. Maverick
+      C. Tucker
+      D. Pepper`,
+    correctAnswer: "D"
+  },
+  {
+    text: `What is my favorite food?
+      A. Tacos
+      B. Hot pot
+      C. KBBQ
+      D. Sushi`,
+    correctAnswer: "B"
+  },
+  {
+    text: `What is my favorite sport?
+      A. Soccer
+      B. Baseball
+      C. Volleyball
+      D. None`,
+    correctAnswer: "D"
+  },
+  {
+    text: `What is my favorite animal?
+      A. Cat
+      B. Elephant
+      C. Snake
+      D. Rabbit`,
+    correctAnswer: "B"
+  },
+  {
+    text: `What am I going to do with my first paycheck?
+      A. Buy my baby a BarkBox
+      B. Buy new clothes
+      C. Put it in my savings
+      D. ALL OF THE ABOVE`,
+    correctAnswer: "D"
+  },
+  {
+    text: `Why am I sad?
+      A. I have no money.
+      B. I have no job.
+      C. I have anxiety.
+      D. All of the above :(`,
+    correctAnswer: "D"
+  },
+  {
+    text: `What is my hobby?
+      A. Archery
+      B. Gaming
+      C. Knitting
+      D. Exercising`,
+    correctAnswer: "B"
+  },
+  {
+    text: `How old am I?
+      A. 20
+      B. 22
+      C. 25
+      D. 27`,
+    correctAnswer: "C"
+  },
+  {
+    text: `What is my old boy's name?
+      A. Jude
+      B. Maverick
+      C. Jax
+      D. Sparky`,
+    correctAnswer: "A"
+  }
+]
